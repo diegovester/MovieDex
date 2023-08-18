05083241a160557dd3f60151c1b352bf782e00c6
Diego Vester <diego.vester@outlook.com>
2023-08-17 19:46:06 -0500

    Initial Commit

---
+++ MovieDex/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MovieDex
+//
+//  Created by Diego Vester on 8/17/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MovieDex/MovieDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDexApp.swift
+//  MovieDex
+//
+//  Created by Diego Vester on 8/17/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
