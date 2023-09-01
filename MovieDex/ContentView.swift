30f8ffb4efd4ad0ca7ab96d6e97d8e93a7cba4e0
Diego Vester <diego.vester@outlook.com>
2023-09-01 16:57:53 -0500

    feat: Bind item variable selectedItem

--- MovieDex/ContentView.swift
+++ MovieDex/ContentView.swift
@@ -7,14 +7,12 @@
 
 import SwiftUI
 
-//
 
 
 struct ContentView: View {
     var movie:[MovieItem]
     var moviePrompt: String = "What movie should we watch tonight?"
-    @State var movieTitle: String = "N/A"
-    @State var randomInt:Int = 0
+    @State private var selectedItem:MovieItem = noMovieItem
     var body: some View {
         VStack {
             Text(moviePrompt)
@@ -26,127 +24,10 @@ struct ContentView: View {
                 .background(.thickMaterial, in: Rectangle())
                 .cornerRadius(50)
             Spacer()
-            
-            VStack {
-                HStack
-                {
-                    Image("DVD")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 100)
-                        
-                    
-                    Text(movie[randomInt].name)
-                        .font(.title).bold()
-                }
-                VStack
-                {
-                    VStack
-                    {
-                        HStack
-                        {
-                            Spacer()
-                            Text("Platform")
-                                .font(.title2).bold()
-                            Spacer()
-                            Text("Available")
-                                .font(.title2).bold()
-                            Spacer()
-                        }
-                        HStack
-                        {
-                            Spacer()
-                            Text("Netflix")
-                            Spacer()
-                            Text("Unavailable")
-                            Spacer()
-                        }
-                        HStack
-                        {
-                            Spacer()
-                            Text("Hulu")
-                            Spacer()
-                            Text("Unavailable")
-                            Spacer()
-                        }
-                        HStack
-                        {
-                            Spacer()
-                            Text("Disney+")
-                            Spacer()
-                            Text("Unavailable")
-                            Spacer()
-                        }
-                        HStack
-                        {
-                            Spacer()
-                            Text("Prime Video")
-                            Spacer()
-                            Text("Unavailable")
-                            Spacer()
-                        }
-                    }
-                    .padding()
-                    VStack
-                    {
-                        HStack
-                        {
-                            Spacer()
-                            Text("Rose")
-                                .font(.title2).bold()
-                            Spacer()
-                            Spacer()
-                            Text("Diego")
-                                .font(.title2).bold()
-                            Spacer()
-                        }
-                        HStack
-                        {
-                            Text("Watched: Yes")
-                            Spacer()
-                            Text("Watched: No")
-                        }
-                            
-                        HStack
-                        {
-                            Text("Liked: Yes")
-                            Spacer()
-                            Text("Liked: No")
-                        }
-                            
-                        HStack
-                        {
-                            Text("Liked: Yes")
-                            Spacer()
-                            Text("Liked: No")
-                        }
-                    }
-                    .padding()
-                }
-            }
-            .padding()
-            .background(.ultraThickMaterial, in: Rectangle())
-            .cornerRadius(50)
-            .padding()
+            MovieItemView(item:$selectedItem)
             Spacer()
-
-            Button
-            {
-                randomInt = Int.random(in: 1...128)
-                movieTitle = movie[randomInt].name
-            }
-            label:
-            {
-                Text("Roll the dice").bold()
-            }
-            
-                .padding(30)
-                .background(.blue, in: Capsule())
-                .foregroundStyle(.white)
-                
+           
         }
-        
-
     }
 }
 
---
+++ MovieDex/MovieItemView.swift
@@ -0,0 +1,38 @@
+//
+//  MovieRowView.swift
+//  MovieDex
+//
+//  Created by Diego Vester on 8/30/23.
+//
+
+import SwiftUI
+
+
+
+struct MovieItemView: View {
+    @Binding var item:MovieItem
+    var body: some View {
+        
+        HStack
+        {
+            Image("DVD")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 100)
+                
+            
+            //Text(selectedMovie.name)
+            Text(item.name)
+                .font(.title).bold()
+        }
+        .padding()
+        .background(.ultraThickMaterial, in: Rectangle())
+        .cornerRadius(50)
+    }
+}
+
+struct MovieItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieItemView(item: .constant(testMovieItem))
+    }
+}
